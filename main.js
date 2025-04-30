6d6aacb003638b054b1a610c991c6d2b924a4453
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 02:16:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
+import * as PIXI from 'https://cdn.skypack.dev/pixi.js';
 import { CelestialChillGame } from './game.js';
 
-const game = new CelestialChillGame(); // it handles its own loading
-
+const game = new CelestialChillGame(PIXI); // pass PIXI explicitly if needed
