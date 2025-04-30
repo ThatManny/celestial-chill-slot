850b36e6bd133c27cf96173b550a537610fb4e84
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 01:44:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import { CelestialChillGame } from './game.js';
 
-const game = new CelestialChillGame();
-game.init();
+PIXI.Loader.shared
+  .add("W1", "assets/wild_pheonix.png")
+  .add("S1", "assets/scatter_wings.png")
+  .add("F1", "assets/golden_feather.png")
+  .add("L1", "assets/feathergreen.png")
+  .add("L2", "assets/featherred.png")
+  .add("L3", "assets/featherpurple.png")
+  .add("L4", "assets/featherblue.png")
+  .load(() => {
+    const game = new CelestialChillGame();
+    game.init();
+  });
 
