f57806b65d7fc4916952d5a6e1261999f72bde96
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 02:09:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 import { CelestialChillGame } from './game.js';
 
-PIXI.Loader.shared
-  .add("W1", "assets/wild_pheonix.png")
-  .add("S1", "assets/scatter_wings.png")
-  .add("F1", "assets/golden_feather.png")
-  .add("L1", "assets/feathergreen.png")
-  .add("L2", "assets/featherred.png")
-  .add("L3", "assets/featherpurple.png")
-  .add("L4", "assets/featherblue.png")
-  .load(() => {
-    const game = new CelestialChillGame();
-    game.init();
-  });
+const game = new CelestialChillGame(); // it handles its own loading
 
