8f836edba808491491cb687a8ad1ecc2d70894f2
ThatManny <manuelvaldeziv@gmail.com>
2025-05-01 00:40:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@7.4.2/dist/browser/pixi.mjs';
 import { CelestialChillGame } from './game.js';
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const game = new CelestialChillGame();
@@ -13,7 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     .add('L3', 'assets/featherpurple.png')
     .add('L4', 'assets/featherblue.png')
     .load((loader, resources) => {
-      // Map each symbol name to its loaded texture
       game.onAssetsLoaded(resources);
       game.init();
     });
