9a5019c37e1534bd8ef929729d49cb4cabacbc20
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 22:50:08 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
+import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@7/+esm';
 
-import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@7/dist/browser/pixi.mjs';
 
 
 export class CelestialChillGame {
