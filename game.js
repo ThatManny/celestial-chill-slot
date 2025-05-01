cbc8ff02fb515a52366614bc1875edc667ef9737
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 23:59:20 -0500

    Update game.js

--- game.js
+++ game.js
@@ -79,8 +79,8 @@ init() {
   spinButton.anchor.set(0.5);
   spinButton.x = this.app.screen.width / 2;
   spinButton.y = 650;
-  spinButton.interactive = true;
-  spinButton.buttonMode = true;
+spinButton.eventMode = 'static';   
+spinButton.buttonMode = true;     
   spinButton.on('pointerdown', () => this.spinReels());
   this.app.stage.addChild(spinButton);
 
