1d050d802f6a388e7b067e16ccaaa6e717853d75
ThatManny <manuelvaldeziv@gmail.com>
2025-04-30 23:45:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
+console.log("▶️ main.js loaded");
+
 import * as PIXI from 'https://cdn.jsdelivr.net/npm/pixi.js@7.4.2/+esm';
 import { CelestialChillGame } from './game.js';
 
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("⚡️ DOMContentLoaded");
+  console.log("▶️ DOMContentLoaded");
   const game = new CelestialChillGame();
-
-  // Bypass assets completely:
   game.init();
 });
